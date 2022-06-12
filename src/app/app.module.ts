999b50c037d1d4e08e32b3fb383ee251dd50114e
Anthonny222 <anthonnespinosa2000@gmail.com>
2022-06-12 14:50:19 -0500

    esqueleto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { WorkComponent } from './work/work.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ExperienceComponent,
+    WorkComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
